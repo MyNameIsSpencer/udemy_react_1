24ceed72bf07a81964d992f415e4a89c120cb08e
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-03-26 12:43:20 -0400

    add gitignore, alter package.json and index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+// import express from 'express';       < This only post ES 2015?
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there'});
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
