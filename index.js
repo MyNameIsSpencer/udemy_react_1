f164e17af5b9fecb57f11fa7399f30619c51d9c2
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-03-26 14:15:32 -0400

    change index message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there'});
+  res.send({ shoulder: 'shrug'});
 });
 
 const PORT = process.env.PORT || 5000;
