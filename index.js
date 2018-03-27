038c75c708af23cce2f2a4670320be6051fdc997
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-03-26 22:34:57 -0400

    add passport

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const express = require('express');
 // import express from 'express';       < This only post ES 2015?
 const app = express();
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/kyes')
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use
+  (new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
