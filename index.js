da00c221c2157afd137d7739700e3632f66e1148
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-03-26 22:54:45 -0400

    fix passport not definded issue?

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 // import express from 'express';       < This only post ES 2015?
-const app = express();
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/kyes')
+const keys = require('./config/keys');
+const PORT = process.env.PORT || 5000;
+const passport = require('passport');
 
 const app = express();
 
-passport.use
-  (new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  },
-    (accessToken) => {
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
       console.log(accessToken);
     }
   )
@@ -25,5 +27,4 @@ app.get(
   })
 )
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT);
